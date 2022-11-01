a9bfc6f181c0f2ca8d6dd693b45b1e7742022065
cipharius <cipharius@pm.me>
2022-11-01 16:01:06 +0200

    Personalizes build.zig

--- build.zig
+++ build.zig
@@ -1,17 +1,10 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("kakoune-arcan", "src/main.zig");
+    const exe = b.addExecutable("aka", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
